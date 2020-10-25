bb9f1aedd4e7e2a7d617bf1984c53b9414f07b53
Vagabondivan <vagabondivan@gmail.com>
2020-10-25 18:36:18 +0600

    Update config.h for each keymap

--- keyboards/bm40hsrgb/config.h
+++ keyboards/bm40hsrgb/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Vagabondivan
+/* Copyright 2020 tominabox1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -45,75 +45,8 @@
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-#define NO_ACTION_MACRO
-
-#define NO_ACTION_FUNCTION
-
-#ifndef NO_DEBUG
-    #define NO_DEBUG
-#endif // !NO_DEBUG
-
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-    #define NO_PRINT
-#endif // !NO_PRINT
-
-/* Maximum time (milliseconds) allowed for TAP-DANCE keys*/
-#define TAPPING_TERM 200
-
-/* Set Autoshift hold time to activate */
-#define AUTO_SHIFT_TIMEOUT 230
-
 #define RGB_DI_PIN E2
-#define RGBLED_NUM 53
 #define DRIVER_LED_TOTAL 53
-
 #ifdef RGB_DI_PIN
-    #define RGBLIGHT_LAYERS
-    #define RGBLIGHT_EFFECT_KNIGHT
-    #define RGBLIGHT_EFFECT_KNIGHT_LENGTH 4
-    #define RGBLIGHT_EFFECT_KNIGHT_OFFSET 14
-    #define RGBLIGHT_EFFECT_KNIGHT_LED_NUM 8
-    #define RGB_DIGITAL_RAIN_DROPS    24
-    #define USB_MAX_POWER_CONSUMPTION 100
-    #define ONESHOT_TIMEOUT           2000
-    #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-    #define RGB_MATRIX_KEYPRESSES                               // reacts to keypresses
-//  #define DISABLE_RGB_MATRIX_ALPHAS_MODS                      // Disables RGB_MATRIX_ALPHAS_MODS
-    #define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN                 // Disables  RGB_MATRIX_GRADIENT_UP_DOWN
-    #define DISABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT              // Disables  RGB_MATRIX_GRADIENT_LEFT_RIGHT
-    #define DISABLE_RGB_MATRIX_BREATHING                        // Disables  RGB_MATRIX_BREATHING
-    #define DISABLE_RGB_MATRIX_BAND_SAT                         // Disables  RGB_MATRIX_BAND_SAT
-    #define DISABLE_RGB_MATRIX_BAND_VAL                         // Disables  RGB_MATRIX_BAND_VAL
-    #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT                // Disables  RGB_MATRIX_BAND_PINWHEEL_SAT
-    #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL                // Disables  RGB_MATRIX_BAND_PINWHEEL_VAL
-    #define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT                  // Disables  RGB_MATRIX_BAND_SPIRAL_SAT
-    #define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL                  // Disables  RGB_MATRIX_BAND_SPIRAL_VAL
-    #define DISABLE_RGB_MATRIX_CYCLE_ALL                        // Disables  RGB_MATRIX_CYCLE_ALL
-    #define DISABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT                 // Disables  RGB_MATRIX_CYCLE_LEFT_RIGHT
-    #define DISABLE_RGB_MATRIX_CYCLE_UP_DOWN                    // Disables  RGB_MATRIX_CYCLE_UP_DOWN
-    #define DISABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON           // Disables  RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-    #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN                     // Disables  RGB_MATRIX_CYCLE_OUT_IN
-    #define DISABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL                // Disables  RGB_MATRIX_CYCLE_OUT_IN_DUAL
-    #define DISABLE_RGB_MATRIX_CYCLE_PINWHEEL                   // Disables  RGB_MATRIX_CYCLE_PINWHEEL
-    #define DISABLE_RGB_MATRIX_CYCLE_SPIRAL                     // Disables  RGB_MATRIX_CYCLE_SPIRAL
-    #define DISABLE_RGB_MATRIX_DUAL_BEACON                      // Disables  RGB_MATRIX_DUAL_BEACON
-    #define DISABLE_RGB_MATRIX_RAINBOW_BEACON                   // Disables  RGB_MATRIX_RAINBOW_BEACON
-    #define DISABLE_RGB_MATRIX_RAINBOW_PINWHEELS                // Disables  RGB_MATRIX_RAINBOW_PINWHEELS
-    #define DISABLE_RGB_MATRIX_RAINDROPS                        // Disables  RGB_MATRIX_RAINDROPS
-    #define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS              // Disables  RGB_MATRIX_JELLYBEAN_RAINDROPS
-//  #define DISABLE_RGB_MATRIX_TYPING_HEATMAP                   // Disables  RGB_MATRIX_TYPING_HEATMAP
-    #define DISABLE_RGB_MATRIX_DIGITAL_RAIN                     // Disables  RGB_MATRIX_DIGITAL_RAIN
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE            // Disables  RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE                   // Disables  RGB_MATRIX_SOLID_REACTIVE
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE              // Disables  RGB_MATRIX_SOLID_REACTIVE_WIDE
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE         // Disables  RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS             // Disables  RGB_MATRIX_SOLID_REACTIVE_CROSS
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS        // Disables  RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-    #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS             // Disables  RGB_MATRIX_SOLID_REACTIVE_NEXUS
-//  #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS        // Disables  RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-    #define DISABLE_RGB_MATRIX_SPLASH                           // Disables  RGB_MATRIX_SPLASH
-    #define DISABLE_RGB_MATRIX_MULTISPLASH                      // Disables  RGB_MATRIX_MULTISPLASH
-    #define DISABLE_RGB_MATRIX_SOLID_SPLASH                     // Disables  RGB_MATRIX_SOLID_SPLASH
-    #define DISABLE_RGB_MATRIX_SOLID_MULTISPLASH                // Disables  RGB_MATRIX_SOLID_MULTISPLASH
+    #define RGB_MATRIX_KEYPRESSES // reacts to keypresses
 #endif
-
