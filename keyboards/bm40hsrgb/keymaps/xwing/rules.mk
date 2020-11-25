0a233be18428e7d727782532d1ac2171b3ca4383
Vagabondivan <vagabondivan@gmail.com>
2020-11-25 22:49:06 +0600

    Custom RGB Matrix Effects added
    Snake, Knight rider, Mirror effects

--- keyboards/bm40hsrgb/keymaps/xwing/rules.mk
+++ keyboards/bm40hsrgb/keymaps/xwing/rules.mk
@@ -1,4 +1,5 @@
 EXTRAFLAGS += -flto
-NKRO_ENABLE = yes			# USB Nkey Rollover
-RGBLIGHT_ENABLE = yes		# Enable keyboard RGB underglow
-TAP_DANCE_ENABLE = yes		# Enable TAP DANCE
+NKRO_ENABLE = yes				# USB Nkey Rollover
+RGBLIGHT_ENABLE = yes			# Enable keyboard RGB underglow
+RGB_MATRIX_CUSTOM_USER = yes	# Enable custom RGB MATRIX effects
+TAP_DANCE_ENABLE = yes			# Enable TAP DANCE
