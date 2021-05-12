41be974bc73d05d977fe70232ba7e9199130c898
Vagabondivan <62350350+Vagabondivan@users.noreply.github.com>
2021-05-12 17:27:47 +0200

    Update keymap.c

--- keyboards/ymdk_np21/keymaps/vgb/keymap.c
+++ keyboards/ymdk_np21/keymaps/vgb/keymap.c
@@ -45,50 +45,155 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_NUMPAD] = LAYOUT_ortho_6x4(
-        LALT(KC_ESC),  LALT(KC_SLSH),  KC_PERC,         RAISE,
-        KC_C,          KC_PSLS,        KC_PAST,         KC_PMNS,
-        KC_P7,         KC_P8,          KC_P9,           KC_PPLS,
-        KC_P4,         KC_P5,          KC_P6,           KC_PPLS,
-        KC_P1,         KC_P2,          KC_P3,           KC_PENT,
-        KC_P0,         KC_DOT,         KC_PDOT,         KC_PENT
-    ),
+ [_NUMPAD] = LAYOUT_ortho_6x4(
+ 
+ /*-------------------------------.
+  | ESC   |       |  %    | FN    |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |  C    |  /    |  *    |  -    |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |  7    |  8    |  9    |       |
+  |       |       |       |       |
+  |-------+-------+-------+   +   |
+  |  4    |  5    |  6    |       |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |  1    |  2    |  3    |   E   |
+  |       |       |       |   N   |
+  |-------+-------+-------+   T   |
+  |  0            |  .    |   E   |
+  |               |       |   R   |
+  `-------------------------------*/ 
+ 
+   LALT(KC_ESC),  LALT(KC_SLSH),  KC_PERC,         RAISE,
+   KC_C,          KC_PSLS,        KC_PAST,         KC_PMNS,
+   KC_P7,         KC_P8,          KC_P9,           KC_PPLS,
+   KC_P4,         KC_P5,          KC_P6,           KC_PPLS,
+   KC_P1,         KC_P2,          KC_P3,           KC_PENT,
+   KC_P0,         KC_P0,          KC_PDOT,         KC_PENT
+),
 
-    [_MACROSA] = LAYOUT_ortho_6x4(
-        PWD1,           PWD2,           KC_NO,          _______,
-        KC_Y,           KC_N,           KC_NO,          KC_BSPC,
-        LNAV1,          DPKG1,          RUDDER1,        KC_L,
-        LNAV2,          DPKG2,          RUDDER2,        KC_ENT,
-        LNAV3,          DPKG3,          RUDDER3,        KC_ENT,
-        KC_NO,          KC_NO,          KC_NO,          KC_ENT
-    ),
+ [_MACROSA] = LAYOUT_ortho_6x4(
+ 
+ /*-------------------------------.
+  |  PWD1 | PWD2  |       |       |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |  Y    |  N    |       | BCKSP |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  | LNAV1 | DPKG1 |RUDDER1|       |
+  |       |       |       |       |
+  |-------+-------+-------+   L   |
+  | LNAV2 | DPKG2 |RUDDER2|       |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  | LNAV3 | DPKG3 |RUDDER3|   E   |
+  |       |       |       |   N   |
+  |-------+-------+-------+   T   |
+  |               |       |   E   |
+  |               |       |   R   |
+  `-------------------------------*/ 
+ 
+   PWD1,           PWD2,           KC_NO,          _______,
+   KC_Y,           KC_N,           KC_NO,          KC_BSPC,
+   LNAV1,          DPKG1,          RUDDER1,        KC_L,
+   LNAV2,          DPKG2,          RUDDER2,        KC_ENT,
+   LNAV3,          DPKG3,          RUDDER3,        KC_ENT,
+   KC_NO,          KC_NO,          KC_NO,          KC_ENT
+),
 
-    [_MACROSB] = LAYOUT_ortho_6x4(
-        PWD1,           PWD2,           KC_NO,          _______,
-        KC_Y,           KC_N,           KC_NO,          KC_BSPC,
-        APT7,           APT8,           APT9,           KC_L,
-        APT4,           APT5,           APT6,           KC_ENT,
-        APT1,           APT2,           APT3,           KC_ENT,
-        KC_NO,          KC_NO,          KC_NO,          KC_ENT
-    ),
+ [_MACROSB] = LAYOUT_ortho_6x4(
+ 
+ /*-------------------------------.
+  |       |       |       |       |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |  Y    |  N    |       | BCKSP |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |  APT7 |  APT8 |  APT9 |       |
+  |       |       |       |       |
+  |-------+-------+-------+   L   |
+  |  APT4 |  APT5 |  APT6 |       |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |  APT1 |  APT2 |  APT3 |   E   |
+  |       |       |       |   N   |
+  |-------+-------+-------+   T   |
+  |               |       |   E   |
+  |               |       |   R   |
+  `-------------------------------*/ 
+ 
+   PWD1,           PWD2,           KC_NO,          _______,
+   KC_Y,           KC_N,           KC_NO,          KC_BSPC,
+   APT7,           APT8,           APT9,           KC_L,
+   APT4,           APT5,           APT6,           KC_ENT,
+   APT1,           APT2,           APT3,           KC_ENT,
+   KC_NO,          KC_NO,          KC_NO,          KC_ENT
+),
 
-    [_RAISE] = LAYOUT_ortho_6x4(
-        NUMPAD,         MACROSA,        MACROSB,        _______,
-        KC_NO,          KC_NO,          KC_NO,          BL_TOGG,
-        KC_NO,          KC_NO,          KC_NO,          BL_INC,
-        KC_NO,          KC_NO,          KC_NO,          BL_INC,
-        ZABBIX1,        ZABBIX2,        ROOT,           BL_DEC,
-        EXIT,           EXIT,           OSL(4),         BL_DEC
-    ),
+ [_RAISE] = LAYOUT_ortho_6x4(
+ 
+ /*-------------------------------.
+  | NUMPD |MACROSA|MACROSB|       |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |       |       |       |  BL   |
+  |       |       |       | TOGG  |
+  |-------+-------+-------+-------|
+  |       |       |       |       |
+  |       |       |       |  BL   |
+  |-------+-------+-------+       |
+  |       |       |       |  INC  |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |ZABBIX1|ZABBIX2| ROOT  |       |
+  |       |       |       |  BL   |
+  |-------+-------+-------+       |
+  |               |OSL(4) |  DEC  |
+  |               |       |       |
+  `-------------------------------*/ 
+ 
+   NUMPAD,         MACROSA,        MACROSB,        _______,
+   KC_NO,          KC_NO,          KC_NO,          BL_TOGG,
+   KC_NO,          KC_NO,          KC_NO,          BL_INC,
+   KC_NO,          KC_NO,          KC_NO,          BL_INC,
+   ZABBIX1,        ZABBIX2,        ROOT,           BL_DEC,
+   EXIT,           EXIT,           OSL(4),         BL_DEC
+),
 
-    [_RESET] = LAYOUT_ortho_6x4(
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_NO,          KC_NO,          KC_NO,
-        KC_NO,          KC_NO,          RESET,          KC_NO
-    )
+ [_RESET] = LAYOUT_ortho_6x4(
+ 
+ /*-------------------------------.
+  |       |       |       |       |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |       |       |       |       |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |       |       |       |       |
+  |       |       |       |       |
+  |-------+-------+-------+       |
+  |       |       |       |       |
+  |       |       |       |       |
+  |-------+-------+-------+-------|
+  |       |       |       |       |
+  |       |       |       |       |
+  |-------+-------+-------+       |
+  |               | RESET |       |
+  |               |       |       |
+  `-------------------------------*/ 
+ 
+   KC_NO,          KC_NO,          KC_NO,          KC_NO,
+   KC_NO,          KC_NO,          KC_NO,          KC_NO,
+   KC_NO,          KC_NO,          KC_NO,          KC_NO,
+   KC_NO,          KC_NO,          KC_NO,          KC_NO,
+   KC_NO,          KC_NO,          KC_NO,          KC_NO,
+   KC_NO,          KC_NO,          RESET,          KC_NO
+)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
