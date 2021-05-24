9d2ad03aa69363602d82284607ade4d2bd009f45
Vagabondivan <62350350+Vagabondivan@users.noreply.github.com>
2021-05-24 17:20:55 +0200

    Update keymap.c

--- keyboards/preonic/keymaps/cbm64/keymap.c
+++ keyboards/preonic/keymaps/cbm64/keymap.c
@@ -1,17 +1,17 @@
 /* CBM64 - Copyright 2020 Ivano Piacenza
  *
- * This program is free software: you can redistribute it and/or modify
+ * This FN is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This FN is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this FN.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include QMK_KEYBOARD_H
@@ -24,7 +24,7 @@ enum layers {
     _LOWER,
     _RAISE,
     _ADJUST,
-    _PROGRAM
+    _FN
 };
 
 // Custom keycodes declarations
@@ -34,7 +34,7 @@ enum keycodes {
     NUMPAD,
     LOWER,
     RAISE,
-    PROGRAM,
+    FN,
     DBLOO,
     MAIL,
     EURO,
@@ -61,63 +61,63 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | TAB  |   q  |   w  |   e  |   r  |   t  |   y  |   u  |   i  |   o  |   p  |   @  |
+ * | Tab  |   q  |   w  |   e  |   r  |   t  |   y  |   u  |   i  |   o  |   p  |   @  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |  =   |   a  |   s  |   d  |   f  |   g  |   h  |   j  |   k  |   l  |   :  |   ;  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | SHFT |   z  |   x  |   c  |   v  |   b  |   n  |   m  |   ,  |   .  |   /  |  Ent |
+ * | Shft |   z  |   x  |   c  |   v  |   b  |   n  |   m  |   ,  |   .  |   /  |  Ent |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | PROG | Ctrl | Alt  |  OS  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * |  Fn  | Ctrl | Alt  |  Os  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_1x2uC (	\
-	KC_ESC,     KC_1,     KC_2,     KC_3,     KC_4,   KC_5,  KC_6,  KC_7,   KC_8,     KC_9,     KC_0,     KC_BSPC,\
-	KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,  KC_Y,  KC_U,   KC_I,     KC_O,     KC_P,     KC_AT,\
-	KC_EQL,     KC_A,     KC_S,     KC_D,     KC_F,   KC_G,  KC_H,  KC_J,   KC_K,     KC_L,     KC_COLN,  KC_SCLN,\
-	TD(CPSLK),  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,  KC_N,  KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,\
-	PROGRAM,    KC_LCTL,  KC_LGUI,  KC_LALT,  LOWER,    KC_SPC,     RAISE,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT\
+	KC_ESC,     KC_P1,    KC_P2,    KC_P3,    KC_P4,  KC_P5,  KC_P6,  KC_P7,  KC_P8,    KC_P9,    KC_P0,    KC_BSPC,\
+	KC_TAB,     KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,     KC_O,     KC_P,     KC_AT,\
+	KC_PEQL,    KC_A,     KC_S,     KC_D,     KC_F,   KC_G,   KC_H,   KC_J,   KC_K,     KC_L,     KC_COLN,  KC_SCLN,\
+	TD(CPSLK),  KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT,\
+	FN,         KC_LCTL,  KC_LGUI,  KC_LALT,  LOWER,      KC_SPC,     RAISE,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT\
 ),
 
 /* Gaming
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   1  |      |   5  |  F1  |  F3  |  F5  |  F7  |  F9  |  F11 |   [  |   ]  |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |      |   W  |      |  F2  |  F4  |  F6  |  F8  |  F10 |  F12 |   '  |Enter |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   A  |   S  |   D  |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | LSft |      |   X  |   C  |   V  |      |      |   M  |   ,  |   .  |  Up  | RSft |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | PROG | LCtl | LAlt | RCtl |Lower |    Space    |Raise | RAlt | Left | Down |Right |
+ * |  Fn  | Ctrl | Alt  |  Os  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
- *///
-[_GAMING] = LAYOUT_preonic_1x2uC (	\
-	KC_ESC,			KC_1,		KC_NO,      KC_5,		KC_F1,		KC_F3,		KC_F5,		KC_F7,		KC_F9,		KC_F11,     KC_LBRC,	KC_RBRC,\
-	KC_TAB,			KC_NO,      KC_W,		KC_NO,      KC_F2,		KC_F4,		KC_F6,		KC_F8,		KC_F10,		KC_F12,     KC_QUOT,  	KC_ENT,\
-	KC_NO,  		KC_A,		KC_S,		KC_D,		KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,\
-	KC_LSFT,        KC_NO,      KC_X,		KC_C,		KC_V,		KC_NO,      KC_NO,      KC_M,		KC_COMM,	KC_DOT,     KC_UP,		KC_RSFT,\
-	PROGRAM,        KC_LCTL,	KC_LALT,	KC_RCTL,    LOWER,      KC_SPC,                 RAISE,      KC_RALT,	KC_LEFT,    KC_DOWN,    KC_RGHT\
+ */
+[_GAMING] = = LAYOUT_preonic_1x2uC (	\
+    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
+    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
+    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
+    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
+	FN,     KC_LCTL,  KC_LGUI,  KC_LALT,  LOWER,     KC_SPC,      RAISE,  KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT\
 ),
 
 /* Numpad
  * ,-----------------------------------------------------------------------------------.
- * |  |   |  !   |  "   |  #   |  $   |  %   |  &   |   '  |  (   |   )  |      | Bksp |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  *   |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   @  |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  ~   |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   [  |   ]  |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   <  |   >  |   ?  |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | PROG | Ctrl | Alt  | Ctrl |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * |  Fn  | Ctrl | Alt  |  Os  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_NUMPAD] = LAYOUT_preonic_1x2uC (	\
-    KC_PIPE,   KC_EXLM,  KC_DQUO,  KC_HASH,  KC_DLR,  KC_PERC,  KC_AMPR,  KC_GRV,  KC_LPRN,  KC_RPRN,  KC_NO,    KC_BSPC,\
-	KC_ASTR,   KC_Q,     KC_W,     KC_E,     KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,     KC_O,     KC_P,     KC_AT,\
-	KC_TILDE,  KC_A,     KC_S,     KC_D,     KC_F,    KC_G,     KC_H,     KC_J,    KC_K,     KC_L,     KC_LBRC,  KC_RBRC,\
-	KC_TRNS,   KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,     KC_N,     KC_M,    KC_LT,    KC_GT,    KC_QUES,  KC_TRNS,\
-	PROGRAM,   KC_LCTL,  KC_LALT,  KC_LGUI,  LOWER,        KC_SPC,        RAISE,   KC_LEFT,	 KC_DOWN,  KC_UP,    KC_RGHT\
+    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
+    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
+    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
+    KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
+	FN,     KC_LCTL,  KC_LGUI,  KC_LALT,  LOWER,     KC_SPC,      RAISE,  KC_LEFT,  KC_DOWN,  KC_UP,   KC_RGHT\
 ),
 
 /* Lower
@@ -130,22 +130,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |Lower |             |Raise | HOME | PGDN | PGUP | END  |
+ * |      |      |      |      |Lower |             |Raise |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_preonic_1x2uC (	\
-    KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,    KC_F9,    KC_F10,   KC_NO,\
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
-    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,\
-    KC_NO,	KC_NO,	KC_NO,  KC_NO,  LOWER,      KC_NO,      RAISE,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END\
+    KC_NO,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,  KC_NO,\
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,\
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,\
+    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,\
+    KC_NO,	KC_NO,	KC_NO,  KC_NO,  LOWER,      KC_NO,      RAISE,  KC_NO,  KC_NO,  KC_NO,   KC_NO,\
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * | BR - | BR + | WBAK | WFWD | BL - | BL + | Prev | Play | Next | Mute | Vol- | Vol+ |
+ * |      | BR - | BR + | WBAK | WFWD | Prev | Play | Next | Mute | Vol- | Vol+ |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   \  |      |      |      |      |      |      |      |      |      |      |   |  |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -155,32 +155,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_1x2uC (	    \
-	KC_BRID,	KC_BRIU,	KC_WBAK,	KC_WFWD,	BL_DEC,		BL_INC,		KC_MPRV,	KC_MPLY,	KC_MNXT,        KC_MUTE,	KC_VOLD,	KC_VOLU,\
-    KC_BSLS,  	KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_NO,      KC_PIPE,\
-    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,          KC_NO,      KC_NO,      KC_NO,\
-    KC_WSTP,    KC_WREF,	KC_WSCH,	KC_PAUS,    KC_PPLS,	KC_PMNS,	KC_UNDS,	KC_PEQL,	SGUI(KC_4),     KC_MUTE,    KC_VOLU,    KC_MPLY,\
-    KC_WBAK,    KC_WHOM,	KC_WFWD,	KC_SLCK,    LOWER,      KC_NO,                  RAISE,      SGUI(KC_3),     KC_MPRV,    KC_VOLD,    KC_MNXT\
+	KC_NO,    KC_BRID,  KC_BRIU,  KC_WBAK,  KC_WFWD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,     KC_VOLD,  KC_VOLU,  KC_NO,\
+    KC_BSLS,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,    KC_NO,    KC_PIPE,\
+    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,       KC_NO,    KC_NO,    KC_NO,\
+    KC_WSTP,  KC_WREF,	KC_WSCH,  KC_PAUS,  KC_PPLS,  KC_PMNS,  KC_UNDS,  KC_PEQL,  SGUI(KC_4),  KC_MUTE,  KC_VOLU,  KC_MPLY,\
+    KC_WBAK,  KC_WHOM,	KC_WFWD,  KC_SLCK,  LOWER,         KC_NO,         RAISE,    SGUI(KC_3),  KC_MPRV,  KC_VOLD,  KC_MNXT\
 ),
 
-/* Program
+/* Fn
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |      | MAIL |      |   €  |      |      |      |      |      |      | Del  |
+ * |CKTOGG|  !   |  "   |  #   |  $   |  %   |  &   |   '  |  (   |   )  |   |  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   *  |      |      |  è   |      |      |      |  ù   |  ì   |  ò   |      |ALT ` |
+ * |   *  |      |      |  è   |      |      |      |  ù   |  ì   |  ò   |      | MAIL |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   à  |      |      |      |      |      |      |      |      |      |NKROTG|
+ * |   ~  |   à  |      |      |      |      |      |      |      |      |   [  |   ]  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |   ñ  |   ¿  |      | MSBL | MS U | MSBR |
+ * |      |      |      |      |      |      |   ñ  |   ¿  |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |CTRL+L|   -  |   +  |Lower |   MENDL'S   |Raise |      | MS L | MS D | MS R |
+ * |      |CTRL+L|   -  |   +  |Lower |   MENDL'S   |Raise | HOME | PGDN | PGUP | END  |
  * `-----------------------------------------------------------------------------------'
  */
-[_PROGRAM] = LAYOUT_preonic_1x2uC (	    \
-	KC_TILD,  KC_NO,       MAIL,     KC_NO,    EURO,   KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_DEL,\
-    KC_ASTR,  KC_NO,       KC_NO,    ACCENTE,  KC_NO,  KC_NO,  KC_NO,    ACCENTU,  ACCENTI,  ACCENTO,  KC_NO,    LALT(KC_GRV),\
-    CK_TOGG,  ACCENTA,     KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,	 KC_NO,    KC_NO,    KC_NO,    KC_NO,    NK_TOGG,\
-    KC_NO,    KC_NO,  	   KC_NO,    KC_NO,    KC_NO,  KC_NO,  SPAIN1,   SPAIN2,   KC_NO,    KC_BTN1,  KC_MS_U,  KC_BTN2,\
-    KC_NO,    LCTL(KC_L),  KC_MINS,  KC_PLUS,  LOWER,       CBM,         RAISE,    KC_NO,    KC_MS_L,  KC_MS_D,  KC_MS_R\
+[_FN] = LAYOUT_preonic_1x2uC (	    \
+	CK_TOGG,  KC_EXLM,     KC_DQUO,  KC_HASH,  KC_DLR,  KC_PERC,  KC_AMPR,  KC_GRV,   KC_LPRN,  KC_RPRN,  KC_PIPE,  KC_DEL,\
+    KC_ASTR,  KC_NO,       KC_NO,    ACCENTE,  KC_NO,   KC_NO,    KC_NO,    ACCENTU,  ACCENTI,  ACCENTO,  KC_NO,    MAIL,\
+    KC_TILD,  ACCENTA,     KC_NO,    KC_NO,    KC_NO,   KC_NO,    KC_NO,	KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,\
+    KC_NO,    KC_NO,  	   KC_NO,    KC_NO,    KC_NO,   KC_NO,    SPAIN1,   SPAIN2,   KC_NO,    KC_NO,    KC_NO,    KC_NO,\
+    KC_NO,    LCTL(KC_L),  KC_MINS,  KC_PLUS,  LOWER,         CBM,          RAISE,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END\
 ),
 
 /* Adjust
@@ -248,12 +248,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
             break;
-        case PROGRAM:
+        case FN:
             if (record->event.pressed) {
-                layer_on(_PROGRAM);
+                layer_on(_FN);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
             } else {
-                layer_off(_PROGRAM);
+                layer_off(_FN);
                 update_tri_layer(_LOWER, _RAISE, _ADJUST);
             }
             return false;
@@ -394,7 +394,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
           knob_code(KC_VOLD);
           break;
         }
-    case _PROGRAM:
+    case _FN:
         if (clockwise) {
           tap_code(KC_RIGHT);
           break;
