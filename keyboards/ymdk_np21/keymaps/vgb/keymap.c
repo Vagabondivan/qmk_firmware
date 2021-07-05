b5d3dfee3a76ee9626af98d757a7e1bdeec04a1b
Vagabondivan <vagabondivan@gmail.com>
2021-07-05 23:01:19 +0200

    Update Keymap

--- keyboards/ymdk_np21/keymaps/vgb/keymap.c
+++ keyboards/ymdk_np21/keymaps/vgb/keymap.c
@@ -17,31 +17,35 @@ enum custom_keycodes {
     MACROSA,
     MACROSB,
     RAISE,
-    PWD1,
-    PWD2,
-    PWD3,
-    PWD4,
-    MAIL,
-    LNAV1,
-    LNAV2,
-    LNAV3,
-    ZABBIX1,
-    ZABBIX2,
-    RUDDER1,
-    RUDDER2,
-    RUDDER3,
     APT1,
+    APT2,
+    APT3,
     APT4,
+    APT5,
+    APT6,
     APT7,
+    APT8,
     DPKG1,
     DPKG2,
     DPKG3,
     DPKG4,
-    APT3,
-    APT6,
-    APT9,
-    APT5,
-    APT8,
+    LNAV1,
+    LNAV2,
+    LNAV3,
+    LNAV4,
+    MAIL,
+    PWD1,
+    PWD2,
+    PWD3,
+    PWD4,
+    RUDDER1,
+    RUDDER2,
+    RUDDER3,
+    RUDDER4,
+    ZABBIX1,
+    ZABBIX2,
+    ZABBIX3,
+    ZABBIX4,
     SYSRST
 
 
@@ -82,60 +86,60 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_MACROSA] = LAYOUT_ortho_6x4 (
 
  /*-------------------------------.
-  | PWD1  | PWD2  | PWD3  |       |
+  | Y     | N     | MAIL  |       |
   |       |       |       |       |
   |-------+-------+-------+-------|
-  | Y     | N     |       | BCKSP |
+  | LNAV4 |ZABBIX4|RUDDER4| BCKSP |
   |       |       |       |       |
   |-------+-------+-------+-------|
-  | LNAV1 |ZABBIX1|RUDDER1|       |
+  | LNAV3 |ZABBIX3|RUDDER3|       |
   |       |       |       |       |
   |-------+-------+-------+   L   |
   | LNAV2 |ZABBIX2|RUDDER2|       |
   |       |       |       |       |
   |-------+-------+-------+-------|
-  | LNAV3 |       |RUDDER3|   E   |
+  | LNAV1 |ZABBIX1|RUDDER1|   E   |
   |       |       |       |   N   |
   |-------+-------+-------+   T   |
   |    CTRL+L     |CTRL+U |   E   |
   |               |       |   R   |
   `-------------------------------*/
 
-   PWD1,           PWD2,           PWD3,           _______,
-   KC_Y,           KC_N,           KC_NO,          KC_BSPC,
-   LNAV1,          ZABBIX1,        RUDDER1,        LCTL(KC_M),
+   KC_Y,           KC_N,           MAIL,           _______,
+   LNAV4,          ZABBIX4,        RUDDER4,        KC_BSPC,
+   LNAV3,          ZABBIX3,        RUDDER3,        LCTL(KC_M),
    LNAV2,          ZABBIX2,        RUDDER2,        KC_ENT,
-   LNAV3,          KC_NO,          RUDDER3,        KC_ENT,
+   LNAV1,          ZABBIX1,        RUDDER1,        KC_ENT,
    LCTL(KC_L),     KC_NO,          LCTL(KC_U),     KC_ENT
 ),
 
  [_MACROSB] = LAYOUT_ortho_6x4 (
 
  /*-------------------------------.
-  |  PWD1 | PWD2  | PWD3  |       |
+  |  Y    |  N    |  MAIL |       |
   |       |       |       |       |
   |-------+-------+-------+-------|
-  |  Y    | DPKG4 |  APT8 | BCKSP |
+  |  APT4 | DPKG4 |  APT8 | BCKSP |
   |       |       |       |       |
   |-------+-------+-------+-------|
-  |  APT7 | DPKG3 |  APT9 |       |
+  |  APT3 | DPKG3 |  APT7 |       |
   |       |       |       |       |
   |-------+-------+-------+ CTRL+ |
-  |  APT4 | DPKG2 |  APT6 |   M   |
+  |  APT2 | DPKG2 |  APT6 |   M   |
   |       |       |       |       |
   |-------+-------+-------+-------|
-  |  APT1 | DPKG1 |  APT3 |   E   |
+  |  APT1 | DPKG1 |  APT5 |   E   |
   |       |       |       |   N   |
   |-------+-------+-------+   T   |
   |    CTRL+L     |CTRL+U |   E   |
   |               |       |   R   |
   `-------------------------------*/
 
-   PWD1,           PWD2,           PWD3,           _______,
-   KC_Y,           DPKG4,          APT8,           KC_BSPC,
-   APT7,           DPKG3,          APT9,           LCTL(KC_M),
-   APT4,           DPKG2,          APT6,           KC_ENT,
-   APT1,           DPKG1,          APT3,           KC_ENT,
+   KC_Y,           KC_N,           MAIL,           _______,
+   APT4,           DPKG4,          APT8,           KC_BSPC,
+   APT3,           DPKG3,          APT7,           LCTL(KC_M),
+   APT2,           DPKG2,          APT6,           KC_ENT,
+   APT1,           DPKG1,          APT5,           KC_ENT,
    LCTL(KC_L),     KC_NO,          LCTL(KC_U),     KC_ENT
 ),
 
@@ -145,8 +149,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   | NUMPD |MACROSA|MACROSB|       |
   |       |       |       |       |
   |-------+-------+-------+-------|
-  |       |       |       |  BL   |
-  |       |       |       | TOGG  |
+  |  PWD1 | PWD2  | PWD3  | PWD4  |
+  |       |       |       |       |
   |-------+-------+-------+-------|
   |       |       |       |       |
   |       |       |       |  BL   |
@@ -157,16 +161,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   |       |       |       |       |
   |       |       |       |  BL   |
   |-------+-------+-------+       |
-  |               |OSL(4) |  DEC  |
+  |   BL TOGGLE   |OSL(4) |  DEC  |
   |               |       |       |
   `-------------------------------*/
 
    NUMPAD,         MACROSA,        MACROSB,        _______,
-   PWD4,           KC_NO,          KC_NO,          BL_TOGG,
+   PWD1,           PWD2,           PWD3,           PWD4,
    KC_NO,          KC_NO,          KC_NO,          BL_INC,
    KC_NO,          KC_NO,          KC_NO,          BL_INC,
    KC_NO,          KC_NO,          KC_NO,          BL_DEC,
-   KC_NO,          KC_NO,          OSL(4),         BL_DEC
+   BL_TOGG,        KC_NO,          OSL(4),         BL_DEC
 ),
 
  [_RESET] = LAYOUT_ortho_6x4 (
@@ -271,7 +275,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case LNAV1:  // Notes
             if (record->event.pressed) {
-                SEND_STRING("lnav /mnt/ACCESSLOG/access.log");
+                SEND_STRING("lnav /var/log/dpkg.log");
             }
             return false;
             break;
@@ -285,28 +289,49 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case LNAV3:  // Notes
             if (record->event.pressed) {
-                SEND_STRING("lnav /var/log/dpkg.log");
+                SEND_STRING("lnav /mnt/ACCESSLOG/access.log");
+            }
+            return false;
+            break;
+
+        case LNAV4:  // Notes
+            if (record->event.pressed) {
+                SEND_STRING("");
             }
             return false;
             break;
 
         case ZABBIX1:  // Notes
             if (record->event.pressed) {
-                SEND_STRING("mkdir /run/zabbix/; chown zabbix /run/zabbix; systemctl restart zabbix-agent");
+                SEND_STRING("/usr/sbin/nginx -t 2>/tmp/nginxLogConfigTest ; echo $? > /tmp/nginxConfigTest");
             }
             return false;
             break;
 
         case ZABBIX2:  // Notes
             if (record->event.pressed) {
-                SEND_STRING("/usr/sbin/nginx -t 2>/tmp/nginxLogConfigTest ; echo $? > /tmp/nginxConfigTest");
+                SEND_STRING("mkdir /run/zabbix/; chown zabbix /run/zabbix; systemctl restart zabbix-agent");
+            }
+            return false;
+            break;
+
+        case ZABBIX3:  // Notes
+            if (record->event.pressed) {
+                SEND_STRING("");
+            }
+            return false;
+            break;
+
+        case ZABBIX4:  // Notes
+            if (record->event.pressed) {
+                SEND_STRING("");
             }
             return false;
             break;
 
         case RUDDER1:  // Notes
             if (record->event.pressed) {
-                SEND_STRING("sudo rudder agent update -f");
+                SEND_STRING("sudo rudder agent update -f && sudo rudder agent run -i");
             }
             return false;
             break;
@@ -320,7 +345,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case RUDDER3:  // Notes
             if (record->event.pressed) {
-                SEND_STRING("sudo rudder agent update -f && sudo rudder agent run -i");
+                SEND_STRING("sudo rudder agent update -f");
+            }
+            return false;
+            break;
+
+        case RUDDER4:  // Notes
+            if (record->event.pressed) {
+                SEND_STRING("");
             }
             return false;
             break;
@@ -332,16 +364,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
             break;
 
-        case APT4:  // Notes
+        case APT2:  // Notes
             if (record->event.pressed) {
                 SEND_STRING("sudo apt-mark hold ");
             }
             return false;
             break;
 
-        case APT7:  // Notes
+        case APT3:  // Notes
             if (record->event.pressed) {
                 SEND_STRING("sudo apt-get upgrade");
+            }
+            return false;
+            break;
+
+        case APT4:  // Notes
+            if (record->event.pressed) {
+                SEND_STRING("sudo apt install ");
             }
             return false;
             break;
@@ -374,7 +413,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
             break;
 
-        case APT3:  // Notes
+        case APT5:  // Notes
             if (record->event.pressed) {
                 SEND_STRING("sudo apt list --upgradable");
             }
@@ -388,7 +427,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
             break;
 
-        case APT9:  // Notes
+        case APT7:  // Notes
             if (record->event.pressed) {
                 SEND_STRING("sudo apt-get update --fix-missing");
             }
@@ -398,13 +437,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case APT8:  // Notes
             if (record->event.pressed) {
                 SEND_STRING("sudo apt --only-upgrade install");
-            }
-            return false;
-            break;
-
-        case APT5:  // Notes
-            if (record->event.pressed) {
-                SEND_STRING("sudo apt install ");
             }
             return false;
             break;
