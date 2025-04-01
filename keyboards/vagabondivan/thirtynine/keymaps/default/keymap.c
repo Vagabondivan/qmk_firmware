4722b8cf38595b0ee197c0ef477c96c20a0bd01f
Vagabondivan <62350350+Vagabondivan@users.noreply.github.com>
2025-04-01 15:42:06 +0200

    Update keymap.c

--- keyboards/vagabondivan/thirtynine/keymaps/default/keymap.c
+++ keyboards/vagabondivan/thirtynine/keymaps/default/keymap.c
@@ -93,10 +93,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  |      |      |  ADJ |      |      ~      | MS L | MS D | MS U | MS R |
  `--------------------------------------------------------------------*/
 
-  KC_ESC,  KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,  KC_NO,       KC_NO,       KC_NO,       KC_DEL,
-  KC_NO,   KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,  KC_NO,       KC_MS_BTN1,  KC_MS_BTN2,  KC_NO,
-  KC_NO,   KC_NO,  KC_NO,   KC_NO,  KC_NO ,  KC_NO,  KC_WH_L,     KC_WH_D,     KC_WH_UP,    KC_WH_R,
-  KC_NO,   KC_NO,  ADJUST,  KC_NO,      KC_TILD,     KC_MS_LEFT,  KC_MS_DOWN,  KC_MS_UP,    KC_MS_RIGHT
+  KC_ESC,  KC_NO,  KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_NO,       KC_NO,       KC_NO,       KC_DEL,
+  KC_NO,   KC_NO,  KC_NO,    KC_NO,  KC_NO,   KC_NO,  KC_NO,       KC_MS_BTN1,  KC_MS_BTN2,  KC_NO,
+  KC_NO,   KC_NO,  KC_NO,    KC_NO,  KC_NO ,  KC_NO,  KC_WH_L,     KC_WH_D,     KC_WH_UP,    KC_WH_R,
+  KC_NO,   KC_NO,  KC_TRNS,  KC_NO,      KC_TILD,     KC_MS_LEFT,  KC_MS_DOWN,  KC_MS_UP,    KC_MS_RIGHT
 ),
 
 [_RAISE] = LAYOUT_ortho_4x10uC(
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_BRID,  KC_BRIU,  SGUI(KC_3),  SGUI(KC_4),  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,
   KC_F1,    KC_F2,    KC_F3,       KC_F4,       KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
   KC_F11,   KC_NO,    KC_NO,       KC_NO,       KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_F12,
-  KC_NO,    ADJUST,   KC_NO,       KC_NO,            KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO
+  KC_NO,    KC_TRNS,  KC_NO,       KC_NO,            KC_NO,         KC_NO,    KC_NO,    KC_NO,    KC_NO
 ),
 
 [_ADJUST] = LAYOUT_ortho_4x10uC(
