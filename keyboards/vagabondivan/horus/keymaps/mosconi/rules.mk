b89e6506458c04dbbcdf0776230e1b7804440b65
Vagabondivan <62350350+Vagabondivan@users.noreply.github.com>
2025-04-01 17:33:09 +0200

    Update rules.mk

--- keyboards/vagabondivan/horus/keymaps/mosconi/rules.mk
+++ keyboards/vagabondivan/horus/keymaps/mosconi/rules.mk
@@ -51,24 +51,7 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 # Build Options
 #   change yes to no to disable
 #
-TAP_DANCE_ENABLE = yes      # Tapping Keys (pressing more than once)
 BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
+TAP_DANCE_ENABLE = yes      # Tapping Keys (pressing more than once)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = no         # Console for debug(+400)
-COMMAND_ENABLE = no         # Commands for debug and configuration
-
-# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = no            # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality on B7 by default
-RGBLIGHT_ENABLE ?= no
-AUDIO_ENABLE = no           # Audio output on port C6
-MIDI_ENABLE = no            # MIDI support (+2400 to 4200, depending on config)
-FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
-UNICODE_ENABLE  = no        # Unicode
-BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 ENCODER_ENABLE = yes        # Activate Encoder
